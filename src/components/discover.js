4b5074b52642e4f39a0d6c446fcb2c8a8285f66a
Michael Thomas <m-thomas89@hotmail.com>
2020-12-10 10:18:26 -0500

    readme

---
+++ src/components/discover.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Discover extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+            <h1>Make New Friends</h1>
+            <h2>Thumbs up on any pups that you'd like to meet</h2>
+
+        </div>
+        {/* <SearchForm
+          search={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <ResultList results={this.state.results} /> */}
+      </div>
+    );
+  }
+}
+
+export default Discover;
+
